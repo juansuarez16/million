a8173848647e5de16765f8fd5dd44f00ccb843c4
juansuarez16 <juancho_1695@hotmail.com>
2025-08-19 01:20:56 -0500

    ajuste next.config

--- million-front/next.config.ts
+++ million-front/next.config.ts
@@ -1,15 +1,15 @@
-import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "picsum.photos" },
-      { protocol: "https", hostname: "placehold.co" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "source.unsplash.com" },
-      // agrega más si cambias de host en el futuro
-    ],
+      { protocol: "https", hostname: "picsum.photos",        pathname: "/**" },
+      { protocol: "https", hostname: "placehold.co",         pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com",  pathname: "/**" },
+      { protocol: "https", hostname: "source.unsplash.com",  pathname: "/**" },
+    ]
   },
 };
 
